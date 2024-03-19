1e124c8f91e9f07b95aa219cc4e5e4d6da1242c7
BitterOcean <maryamsaeedmehr@gmail.com>
2024-03-20 02:40:29 +0330

    update url

--- src/constants/routes.jsx
+++ src/constants/routes.jsx
@@ -1,2 +1,2 @@
 export const API_ROOT = 'https://covid-19-detector-model.herokuapp.com';
-export const API_IMAGE_UPLOAD = `${API_ROOT}/api/posts/`;
+export const API_IMAGE_UPLOAD = `${API_ROOT}/api/predict/`;
